45e5bc816e97174a025b2bdfb80f8d0297b3298f
SerG2afk <118850063+SerG2afk@users.noreply.github.com>
2024-12-18 10:51:02 +0100

    Add files via upload

--- lesson_18/js/script.js
+++ lesson_18/js/script.js
@@ -1,9 +1,11 @@
-let documentActions = (e) => {
-	const targetElement = e.target
-
-	if (targetElement.closest('.icon-menu')) {
-		document.documentElement.classList.toggle('open-menu')
-	}
-}
-
-document.addEventListener('click', documentActions)
+//=================================================
+// burger-menu
+document.addEventListener("click", function (e) {
+    const targetElement = e.target;
+    // .icon-menu це клас кнопки при кліку на яку буде спрацьовувати код додаватись до тегу html клас menu-open 
+    if (targetElement.closest('.icon-menu')) {
+      document.documentElement.classList.toggle('open-menu');
+      e.preventDefault();
+    }
+  })
+  //=================================================
