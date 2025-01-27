5a175ff9eb9c8e1c62a43eaf2feeecfd5eb4f2ad
SerG2afk <118850063+SerG2afk@users.noreply.github.com>
2025-01-27 14:55:22 +0100

    Add files via upload

--- lesson_21/js/script.js
+++ lesson_21/js/script.js
@@ -9,3 +9,31 @@ document.addEventListener("click", function (e) {
     }
   })
   //=================================================
+
+//   const listItem = document.querySelectorAll(`.menu__link`)
+//   console.log(listItem)
+//   listItem.forEach((listItem, index)  => {
+//   // console.log(listItem.innerHTML)
+//   listItem.textContent = `<span> Ку № ${index} </span>`
+// })
+
+// let newElement = document.createElement(`div`)
+// newElement.innerHTML = `<span> Хай </span>`
+// console.log(newElement)
+
+// const page = document.querySelector(`.main`)
+// // (`.main`).before(newElement)
+// page.prepend(newElement)
+
+
+// const page = document.querySelector(`.header`)
+
+// let template = `<div class="block">`
+// if (page) {
+//   template += `<span> Хай </span>`
+// }
+// template += `</div>`
+// console.log(template)
+
+// // page.insertAdjacentHTML("beforeend", template)
+// page.insertAdjacentHTML("beforeend", `<div class="block"<span> Хай </span></div>`)
